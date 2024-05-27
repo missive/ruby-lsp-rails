c0d105c3b398108fc881c60424037b7bdc586ae7
Louis-Michel Couture <louim@missiveapp.com>
2024-05-27 10:15:34 -0400

    Use our custom `bin/dev` wrapper instead of `bundle exec`
    This is required until https://github.com/Shopify/ruby-lsp-rails/issues/389 is resolved.

--- lib/ruby_lsp/ruby_lsp_rails/runner_client.rb
+++ lib/ruby_lsp/ruby_lsp_rails/runner_client.rb
@@ -71,7 +71,7 @@ module RubyLsp
         log_message("Ruby LSP Rails booting server")
 
         stdin, stdout, stderr, wait_thread = Bundler.with_original_env do
-          Open3.popen3("bundle", "exec", "rails", "runner", "#{__dir__}/server.rb", "start")
+          Open3.popen3("bin/dev", "rails", "runner", "#{__dir__}/server.rb", "start")
         end
 
         @stdin = T.let(stdin, IO)
